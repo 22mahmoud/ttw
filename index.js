ca3cfe634ed5c6eac5823cec89d1cd695e14c666
22mahmoud <hello@mahmoudashraf.dev>
2023-11-15 01:40:48 +0200

    hello world api

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-console.log("Hello, World");
+import { request } from "node:https";
 
+const options = {
+  origin: "https",
+  host: "api.telegram.org",
+  path: `/bot${process.env.TOKEN}/getMe`,
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+  },
+};
+
+const req = request(options, (res) => {
+  let rawData = "";
+  res
+    .on("data", (chunk) => void (rawData += chunk))
+    .on("end", () => {
+      const parsedData = JSON.parse(rawData);
+      console.log(parsedData);
+    });
+}).on("error", (e) => {
+  console.error(e);
+  console.error(`Got error: ${e.message}`);
+});
+
+req.end();
